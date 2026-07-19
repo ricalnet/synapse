c24fdbf580f595871382eb430ed11de1a7ef300e
ricalnet <ricalnet@users.noreply.github.com>
2026-07-19 12:19:54 +0000

    deploy: 5ed830b3b4c74c89d876cc07756c5d98a100cbed

--- develop/searcher-c2a407aa.js
+++ develop/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-14280e6f.js',
+                path_to_root + 'searchindex-563d6fdf.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
